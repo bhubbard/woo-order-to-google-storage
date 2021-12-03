d13cd69158599cf84df25c01bf9b55b01a676135
Brandon Hubbard <bhubbard@users.noreply.github.com>
2021-12-03 17:01:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.saveOrder = (event, context) => {
+  const {Storage} = require('@google-cloud/storage');
+  const storage = new Storage()
+  const bucket = storage.bucket('accessnetworks-woocommerce-orders')
+
+  // Parse PubSub message.
+  const pubsubMessage = JSON.parse(Buffer.from(event.data, 'base64').toString());
+  
+  // Define Message
+  const order = pubsubMessage.data.message;
+  var order_id = order.id;
+    console.log( order_id );
+    console.log(order);
+
+    const timestamp = new Date().getTime()
+    const fileName = order_id + `/${timestamp}.json`
+    const file = bucket.file(fileName)
+    const contents = JSON.stringify(order)
+    return file.save(contents)
+
+};
